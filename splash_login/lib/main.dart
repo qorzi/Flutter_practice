f13d9cabf448b1cb17d8ba04681cf71e24d55be9
qorzi00 <qorzi00@gmail.com>
2023-03-12 22:36:08 +0900

    스플래시 및 로딩, 분기 추가

---
+++ splash_login/lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: SplashScreen(),
+    ),
+  );
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _isMoved = false;
+  bool _isVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // 1초 후에 _isMoved 값을 변경하여 애니메이션을 발생시킴
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _isMoved = true;
+      });
+      Future.delayed(Duration(milliseconds: 500), () {
+        setState(() {
+          _isVisible = true;
+        });
+        // _navigateToNextScreen();
+      });
+      Future.delayed(Duration(seconds: 3), () {
+        _navigateToNextScreen();
+      });
+    });
+  }
+
+  void _navigateToNextScreen() {
+    final isLoggedIn = false; // 여기에 로그인 상태를 확인하는 로직을 구현합니다.
+
+    if (isLoggedIn) {
+      Route home = MaterialPageRoute(builder: (context) => HomeScreen());
+      Navigator.pushReplacement(context, home); // 로그인 상태이면 홈화면으로 이동합니다.
+    } else {
+      // Route login = MaterialPageRoute(builder: (context) => LoginScreen());
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          // 연결할 페이지
+          pageBuilder: (c, a1, a2) => LoginScreen(),
+          // 적용할 애니메이션
+          transitionsBuilder: (c, a1, a2, child) => FadeTransition(opacity: a1, child: child),
+          // 적용 시간 설정
+          transitionDuration: Duration(milliseconds : 500),)); // 로그인 상태가 아니면 로그인화면으로 이동합니다.
+          }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFD91604),
+        child: Stack(
+          children: [
+            Center(
+              child: TweenAnimationBuilder<double>(
+                duration: Duration(milliseconds: 500),
+                tween: Tween<double>(begin: 0, end: _isMoved ? -100 : 0),
+                builder: (BuildContext context, double value, Widget? child) {
+                  return Transform.translate(
+                    offset: Offset(0, value),
+                    child: child,
+                  );
+                },
+                child: Image.asset(
+                  'assets/main_logo_white_x1.png',
+                  // width: 200,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 300,
+              left: MediaQuery.of(context).size.width / 2 - 18,
+              child: Visibility(
+                visible: _isVisible,
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome Home!'),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFF2E4D9),
+        child: Stack(
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).size.height / 2 - 191,
+              left: MediaQuery.of(context).size.width / 2 - 109,
+              child: Image.asset(
+                'assets/main_logo_red_x1.png',
+                // width: 200,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
